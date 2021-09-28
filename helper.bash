1b553ee68782a133a2922f78e6be1c229002534a
Andrey Morozov <60394118+aandrey5@users.noreply.github.com>
2021-09-28 09:00:44 +0300

    Update helper.bash

--- helper.bash
+++ helper.bash
@@ -2,3 +2,16 @@
 sudo sysctl fs.protected_regular=0
 
 root@andrey:~# minikube start --vm-driver=none
+
+# control test
+
+kubectl apply -f https://k8s.io/examples/controllers/nginx-deployment.yaml
+
+root@andrey:~# kubectl apply -f https://k8s.io/examples/controllers/nginx-deployment.yaml
+deployment.apps/nginx-deployment created
+root@andrey:~# kubectl get deployments
+NAME               READY   UP-TO-DATE   AVAILABLE   AGE
+nginx-deployment   3/3     3            3           19s
+
+
+
